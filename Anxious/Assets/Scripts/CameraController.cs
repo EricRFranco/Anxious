3fa0dfec04d9ca7223cf14c5ce5c82ac39dcf605
EricRFranco <ericrfranco219@gmail.com>
2017-05-06 11:45:02 -0400

    Simpler movement system implemented and free roaming camera. Need to implement hiding spots.

--- Anxious/Assets/Scripts/CameraController.cs
+++ Anxious/Assets/Scripts/CameraController.cs
@@ -4,51 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Public variables
-	[SerializeField]
-	private float _minPitch;
-	[SerializeField]
-	private float _maxPitch;
-	[SerializeField]
-	private float _pitchSpeed;
+	public float horizontalSpeed = 200f;
+	public float verticalSpeed = 200f;
 
-	// System variables
-	public float CurrentPitch
-	{
-		get { return transform.localRotation.eulerAngles.x; }
-	}
-
-	// Private interface
-	private Camera camera_;
-
-	// Initialization
-	public void Awake()
-	{
-		camera_ = GetComponent<Camera>();
-	}
-
-	// Public interface
-	public void Pitch(float pitchInput)
-	{
-		Vector3 localRotation = transform.localRotation.eulerAngles;
-		localRotation.x -= (localRotation.x > 180.0f ? 360.0f : 0.0f);
-		localRotation.x = (Mathf.Clamp(localRotation.x + pitchInput * _pitchSpeed * Time.deltaTime, _minPitch, _maxPitch) + 360.0f) % 360.0f;
-		localRotation.y = 0.0f;
-		localRotation.z = 0.0f;
-		transform.localRotation = Quaternion.Euler(localRotation);
-	}
-	public bool CanSee(Vector3 point)
-	{
-		Vector3 screenPoint = camera_.WorldToScreenPoint(point);
-		RaycastHit hit;
-		bool contact = Physics.Raycast(transform.position, (point - transform.position).normalized, out hit);
-		return (
-			screenPoint.x >= 0 &&
-			screenPoint.y >= 0 &&
-			screenPoint.x < camera_.pixelWidth &&
-			screenPoint.y < camera_.pixelHeight &&
-			screenPoint.z > 0.0f &&
-			(!contact || (hit.point - transform.position).magnitude >= (point - transform.position).magnitude - 0.1f)
-		);
+	void Update() {
+		float h = horizontalSpeed * Input.GetAxis ("Mouse X");
+		float v = verticalSpeed * Input.GetAxis ("Mouse Y");
+		transform.Rotate(v, h, 0);
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
 	}
 }
--- Anxious/Assets/Scripts/PlayerController.cs
+++ Anxious/Assets/Scripts/PlayerController.cs
@@ -4,121 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Static variables
-	public static PlayerController instance;
+	public float speed = 2f;
+	GameObject player;
 
-	// Public variables
-	[Header("Movement")]
-	[SerializeField]
-	private float _speed;
-	[SerializeField]
-	private float _turnSpeed;
-	[SerializeField]
-	private float _gravity;
-	[SerializeField]
-	private float _jumpVelocity;
-
-	[Header("References")]
-	[SerializeField]
-	private CameraController _cameraController;
-
-	// System variables
-	public float CurrentPitch
-	{
-		get { return _cameraController.CurrentPitch; }
-	}
-	public bool IsLookingAtInteractable
-	{
-		get { return _isLookingAtInteractable; }
+	void Start() {
+		player = this.gameObject;
 	}
 
-	// Private variables
-	private CharacterController _characterController;
-	private float _verticalVelocity;
-	private bool _isLookingAtInteractable;
-	private Vector3 _startingPosition;
-
-	// Initialization
-	public void Awake()
-	{
-		instance = this;
-		_characterController = GetComponent<CharacterController>();
-		_verticalVelocity = 0.0f;
-		_isLookingAtInteractable = false;
-		_startingPosition = transform.position;
-	}
-
-	// Update
-	public void Update()
-	{
-		Vector2 movementInput = InputManager.GetFirstPersonMovement();
-		Vector2 cameraInput = InputManager.GetCameraMovement();
-
-		// Turn by horizontal
-		float turnAmount = cameraInput.x * _turnSpeed * Time.deltaTime;
-		transform.Rotate(Vector3.up, turnAmount);
-
-		// Pitch by vertical
-		_cameraController.Pitch(-cameraInput.y);
-
-		// Move relative to orientation
-		Vector3 movement = (transform.right * movementInput.x + transform.forward * movementInput.y) * _speed;
-		if (_characterController.isGrounded)
-		{
-			RaycastHit hit;
-			if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.1f))
-			{
-				transform.SetParent(hit.transform, true);
-			}
-			else
-			{
-				transform.SetParent(null);
-			}
-
-			_verticalVelocity = 0.0f;
-			if (Input.GetButton("Jump"))
-			{
-				_verticalVelocity = _jumpVelocity;
-				transform.SetParent(null);
-			}
-		}
-		_verticalVelocity += _gravity * Time.deltaTime;
-		movement.y = _verticalVelocity;
-		_characterController.Move(movement * Time.deltaTime);
-
-		// Update interactables
-		Interactable interactable = LookForInteractable();
-		_isLookingAtInteractable = (interactable != null);
-		if (_isLookingAtInteractable && InputManager.GetInteract())
-			interactable.Interact();
-
-		// Respawn on kill
-		if (transform.position.y < -5.0f)
-			transform.position = _startingPosition;
-	}
-
-	// Public interface
-	public bool CanSee(Vector3 point)
-	{
-		return _cameraController.CanSee(point);
-	}
-
-	// Private interface
-	private Interactable LookForInteractable()
-	{
-		RaycastHit hit;
-		if (Physics.Raycast(_cameraController.transform.position, _cameraController.transform.forward, out hit, 100.0f))
-		{
-			Interactable interactable = hit.collider.GetComponent<Interactable>();
-			if (interactable == null)
-				return null;
-
-			Vector3 toInteractable = _cameraController.transform.position - interactable.transform.position;
-			float distanceToInteractable = Vector3.ProjectOnPlane(toInteractable, Vector3.up).magnitude;
-			if (distanceToInteractable < interactable.interactableDistance)
-				return interactable;
-		}
-
-		return null;
+	void Update() {
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
 }
