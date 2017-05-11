c853337293e1c44066e996ce25abd8c50f81fef6
EricRFranco <ericrfranco219@gmail.com>
2017-05-10 21:24:02 -0400

    Began implementing player script and adding hiding places. System update preventing further additions at the moment.

--- Anxious/Assets/Scripts/ConfidenceController.cs
+++ Anxious/Assets/Scripts/ConfidenceController.cs
@@ -7,23 +7,13 @@ public class ConfidenceController : MonoBehaviour {
 
 	private Slider _confidence;
 	private int _currentConfidence = 100;
-	private float _timeLeft = 3f;
 
-	void Awake () {
-		
+	private void Awake() {
 		_confidence = GetComponent<Slider> ();
+	}
 
-		}
-
-	void Update() {
-
+	private void Update() {
 		_confidence.value = _currentConfidence;
-		_timeLeft -= Time.deltaTime;
-		if (_timeLeft <= 0) {
-			ChangeConfidence (-5);
-			_timeLeft = 30;
-		}
-
 	}
 
 	public void ChangeConfidence (int dC) {
---
+++ Anxious/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	[HideInInspector]
+	public bool isHidden = false;
+	public ConfidenceController confidence;
+	public float speed = 2f;
+
+	[SerializeField]
+	private float _timeLeft = 3f;
+	//private GameObject [] _hidingSpots;
+
+	void Awake() {
+		//_hidingSpots = GameObject.FindGameObjectsWithTag ("Hide");
+	}
+
+	void Update() {
+		if (!isHidden) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+			_timeLeft -= Time.deltaTime;
+			if (_timeLeft <= 0) {
+				confidence.ChangeConfidence (-5);
+				_timeLeft = 3f;
+			}
+		}
+	}
+}
