ca6896f610da4a22337e4ab0c6494f409464ecde
EricRFranco <ericrfranco219@gmail.com>
2017-05-11 12:06:40 -0400

    Working on implementing hiding places. Requires further testing but pushing in case something goes wrong.

--- Anxious/Assets/Scripts/Player.cs
+++ Anxious/Assets/Scripts/Player.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class Player : MonoBehaviour {
 
 	[HideInInspector]
-	public bool isHidden = false;
+	public bool isHidden;
 	public ConfidenceController confidence;
 	public float speed = 2f;
 
 	[SerializeField]
 	private float _timeLeft = 3f;
-	//private GameObject [] _hidingSpots;
+	private GameObject [] _hidingSpots;
+	private List<GameObject> _filterHidden;
 
-	void Awake() {
-		//_hidingSpots = GameObject.FindGameObjectsWithTag ("Hide");
+	private void Awake() {
+		_hidingSpots = GameObject.FindGameObjectsWithTag ("Hide");
+		_filterHidden = new List<GameObject> ();
+		foreach (GameObject go in _hidingSpots) {
+			_filterHidden.Add (go);
+		}
+		isHidden = false;
 	}
 
-	void Update() {
+	private void Update() {
+		FilterHidingSpots ();
+
+		Transform closest = ClosestHidingPlace (_filterHidden).transform;
+		if (Input.GetMouseButton (0) && transform.position.x == closest.position.x) {
+			isHidden = true;
+			StartCoroutine ("DelayCountdown");
+			transform.position = Vector3.MoveTowards (transform.position, closest.position, speed * Time.deltaTime);
+		} else if (!Input.GetMouseButton (0)) {
+			isHidden = false;
+		}
+
 		if (!isHidden) {
 			transform.Translate (Vector3.right * speed * Time.deltaTime);
 			_timeLeft -= Time.deltaTime;
@@ -26,6 +44,35 @@ public class Player : MonoBehaviour {
 				confidence.ChangeConfidence (-5);
 				_timeLeft = 3f;
 			}
+		} 
+
+
+	}
+
+	private void FilterHidingSpots () {
+		foreach (GameObject go in _filterHidden) {
+			if (go.transform.position.x < transform.position.x) {
+				_filterHidden.Remove (go);
+			}
+
 		}
+		print (_filterHidden.Count);
+	}
+
+	private GameObject ClosestHidingPlace(List<GameObject> hide) {
+		List<float> distances = new List<float> ();
+		int minIndex;
+		foreach(GameObject go in hide) {
+			distances.Add(Vector3.Distance (transform.position, go.transform.position));
+		}
+
+		minIndex = distances.IndexOf (distances.Min());
+		return hide [minIndex];
+
+	}
+
+	private IEnumerator DelayCountdown () {
+		yield return new WaitForSeconds (5);
+		confidence.ChangeConfidence (-5);
 	}
 }
