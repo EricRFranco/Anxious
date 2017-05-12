089f6a25e46e8fee6d6388bddb6110380bbecb8b
EricRFranco <ericrfranco219@gmail.com>
2017-05-11 20:23:11 -0400

    Car spawning done.

---
+++ Anxious/Assets/Scripts/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	public float speed;
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z > 4f)
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		else
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+		if (transform.position.x < -14f || transform.position.x > 9f) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Anxious/Assets/Scripts/CarSpawner.cs
+++ Anxious/Assets/Scripts/CarSpawner.cs
@@ -12,9 +12,9 @@ public class CarSpawner : Spawner {
 	void Update () {
 		int carIndex = Random.Range (0, cars.Length - 1);
 		carSpawnTime -= Time.deltaTime;
-		if (carSpawnTime <= 0) {
-			Spawn (cars [carIndex], transform.position, Quaternion.identity);
-			carSpawnTime = Random.Range (2f, 5f);
+		if (carSpawnTime <= 0) {  
+			Spawn (cars [carIndex], transform.position, Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0));
+			carSpawnTime = Random.Range (2f, 10f);
 		}
 	}
 }
